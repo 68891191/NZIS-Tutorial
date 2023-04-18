1604882210603aec75e586c916ae1c428474d1d6
warren <warren.zheng@elmosoftware.co.nz>
2023-04-18 15:36:42 +1200

    Seminar 01

---
+++ WebApp/Controllers/UniversityController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class UniversityController : Controller
+    {
+        public IActionResult Index()
+        {
+            University waikatoUniversity = new()
+            {
+                Id = 1,
+                QSRank = 331,
+                Name = "The University of Waikato",
+                Description = "The University of Waikato is committed to delivering a world-class education and research portfolio. With around 13,000 students and 1,500 staff, we offer a distinctive and rewarding university experience, while pursuing strong international links to advance knowledge.",
+                Website = "https://www.waikato.ac.nz/",
+                City = "Hamilton"
+            };
+
+            ViewData["University"] = waikatoUniversity;
+            ViewBag.Univeristy = waikatoUniversity;
+
+            return View();
+        }
+    }
+}
---
+++ WebApp/Models/University.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApp.Models
+{
+    public class University
+    {
+        public int Id { get; set; }
+        public int QSRank { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Website { get; set; }
+        public string City { get; set; }
+    }
+}
